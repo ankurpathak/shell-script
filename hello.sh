dddcc950e587090e93fff3924af91e970683f5e1
Ankur Pathak <adpathak@lentra.ai>
2020-06-18 15:00:08 +0530

    Nested if else file append script

--- hello.sh
+++ hello.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
 echo -e "Enter file name: \c";
 read file
-echo "Entered file name is $file"
-if [ -e "$file" ];
+if [ -n "$file" -a -f "$file" ];
 then
-  echo "file $file exists";
+  if [ -w "$file" ];
+  then
+    echo "Enter some data. Press cntrl+d to exit";
+    cat >> $file;
+  else
+    echo "$file does not have write permission";
+  fi
 else
-  echo "file $file doesn't exist";
+  echo "$file doesn't exist";
 fi
-
-# -e file exists
-# -f file
-# -d directory
-# -s file is not empty
-# -r readable file
-# -w writeable file
-# -x executable file
