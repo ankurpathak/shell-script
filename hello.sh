da09f59127885f8a88817783d87778c44c03b2b0
Ankur Pathak <adpathak@lentra.ai>
2020-06-18 19:47:41 +0530

    Integer Arithmetic Second way

--- hello.sh
+++ hello.sh
@@ -6,3 +6,11 @@ echo $(( "$num1" - "$num2" ))
 echo $(( "$num1" * "$num2" ))
 echo $(( "$num1" / "$num2" ))
 echo $(( "$num1" % "$num2" ))
+
+echo "---------------------"
+
+echo $( expr "$num1" + "$num2" )
+echo $( expr "$num1" - "$num2" )
+echo $( expr "$num1" \* "$num2" )
+echo $( expr "$num1" / "$num2" )
+echo $( expr "$num1" % "$num2" )
