91c2fb1577ef94bc3812037faed19cc023234e4e
Ankur Pathak <adpathak@lentra.ai>
2020-06-18 14:24:27 +0530

    File flags added

--- hello.sh
+++ hello.sh
@@ -2,3 +2,17 @@
 echo -e "Enter file name: \c";
 read file
 echo "Entered file name is $file"
+if [ -e "$file" ];
+then
+  echo "file $file exists";
+else
+  echo "file $file doesn't exist";
+fi
+
+# -e file exists
+# -f file
+# -d directory
+# -s file is not empty
+# -r readable file
+# -w writeable file
+# -x executable file
