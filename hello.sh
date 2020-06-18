4fae062bf0f0e950175a23ecc5364e6c5e66638e
Ankur Pathak <adpathak@lentra.ai>
2020-06-18 19:25:53 +0530

    Integer Arithmetic First Way

--- hello.sh
+++ hello.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-read -p "Enter age: " age
-if [[ "$age" -gt 18 && "$age" -lt 30 ]];
-then
-  echo "$age age is valid";
-else
-  echo "$age age is not valid";
-fi
+num1=20
+num2=5
+echo $(( "$num1" + "$num2" ))
+echo $(( "$num1" - "$num2" ))
+echo $(( "$num1" * "$num2" ))
+echo $(( "$num1" / "$num2" ))
+echo $(( "$num1" % "$num2" ))
