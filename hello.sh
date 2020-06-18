f71fc45480edc12a0ff24c2da27c95ce28abd6b8
Ankur Pathak <adpathak@lentra.ai>
2020-06-18 13:48:20 +0530

    If else conditional added

--- hello.sh
+++ hello.sh
@@ -1,33 +1,8 @@
 #!/bin/bash
-count=10
-if [ "$count" \< '10' ];
+name='ankur';
+if [ "$name" = 'ankur' ];
 then
-  echo "Condition is true";
+  echo 'Condtion is true';
+else
+  echo 'Condition is false';
 fi
-# []
-# variable set
-# -v varible
-
-# string comparison(Use quotes always)
-# = equal
-# != not equal
-# \< less that
-# \> greater that
-# -n not zero length
-# -z zero length
-
-# integer comparison
-# -ne not equal
-# -eq equal
-# -gt greater than
-# -ge greater equal
-# -lt less than
-# -le less than equal
-
-# (()) C Language Integers
-# == equal
-# != not equal
-# > greater than
-# <  less than
-# >= greater equal
-# <= less equal
