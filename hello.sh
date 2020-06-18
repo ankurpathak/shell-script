6ffd36c0339f6e2f2baa3ccefddfc4702ae1cb8a
Ankur Pathak <adpathak@lentra.ai>
2020-06-18 13:32:08 +0530

    Conditional comparison added

--- hello.sh
+++ hello.sh
@@ -1,6 +1,33 @@
 #!/bin/bash
 count=10
-if [ $count -eq 10 ];
+if [ "$count" \< '10' ];
 then
   echo "Condition is true";
 fi
+# []
+# variable set
+# -v varible
+
+# string comparison(Use quotes always)
+# = equal
+# != not equal
+# \< less that
+# \> greater that
+# -n not zero length
+# -z zero length
+
+# integer comparison
+# -ne not equal
+# -eq equal
+# -gt greater than
+# -ge greater equal
+# -lt less than
+# -le less than equal
+
+# (()) C Language Integers
+# == equal
+# != not equal
+# > greater than
+# <  less than
+# >= greater equal
+# <= less equal
