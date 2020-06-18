08fb307ba49b0ba2a5a0ca852e071c908b009207
Ankur Pathak <adpathak@lentra.ai>
2020-06-18 14:03:32 +0530

    Echo same line prompt

--- hello.sh
+++ hello.sh
@@ -1,14 +1,4 @@
 #!/bin/bash
-char='c';
-if [ "$char" = 'a' ];
-then
-  echo 'Character is a';
-elif [ "$char" = 'b' ];
-then
- echo 'Character is b';
-elif [ "$char" = 'c' ];
-then
- echo 'Character is c';
-else
-  echo 'Characted is other';
-fi
+echo -e "Enter file name: \c";
+read file
+echo "Entered file name is $file"
