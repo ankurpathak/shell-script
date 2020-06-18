a0c878b8ed2594b6a8ec96864eb877cca3ced8b3
Ankur Pathak <adpathak@lentra.ai>
2020-06-18 09:45:13 +0530

    Buildin Variables

--- hello.sh
+++ hello.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 # this this is a comment
 echo "Hello World" # this is a comment
+echo "My Shell is $BASH"
+echo "MY Bash Version is $BASH_VERSION"
+echo "MY Present Working Directory is $PWD"
+echo "MY Home is $HOME"
+echo "MY BASH Process Id is $BASHPID"
