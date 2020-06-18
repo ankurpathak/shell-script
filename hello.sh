940b830a2f4343a1d488923f324f17cbec6b836f
Ankur Pathak <adpathak@lentra.ai>
2020-06-18 10:00:52 +0530

    Read variable

--- hello.sh
+++ hello.sh
@@ -1,13 +1,4 @@
 #!/bin/bash
-# this this is a comment
-echo "Hello World" # this is a comment
-echo "My Shell is $BASH"
-echo "MY Bash Version is $BASH_VERSION"
-echo "MY Present Working Directory is $PWD"
-echo "MY Home is $HOME"
-echo "MY BASH Process Id is $BASHPID"
-
-name='Ankur'
-10val=10
-echo "My name is $name"
-echo "10val is $10val"
+echo 'Enter name'
+read name
+echo "Entered name: $name"
