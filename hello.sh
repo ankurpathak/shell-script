fc1328157996e144a51e7471c90ca587bbf2346e
Ankur Pathak <adpathak@lentra.ai>
2020-06-19 09:16:19 +0530

    Capture stdout in variable

--- hello.sh
+++ hello.sh
@@ -6,3 +6,7 @@ a=$(bc <<< " $num1 - $num2 "); echo "$a"
 a=$(bc <<< " $num1 * $num2 "); echo "$a"
 a=$(bc <<< " scale=2; $num1 / $num2 "); echo "$a"
 a=$(bc <<< " $num1 % $num2 "); echo "$a"
+
+# Catch stdout in variable
+# cat file - gives stdout
+# a=$(cat file)
