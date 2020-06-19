3804a33aadbc8070489ea29481a1fdfef3e8ce71
Ankur Pathak <adpathak@lentra.ai>
2020-06-19 09:20:14 +0530

    Calculate sqrt

--- hello.sh
+++ hello.sh
@@ -1,12 +1,3 @@
 #!/bin/bash
-num1=20.5
-num2=5
-a=$(bc <<< " $num1 + $num2 "); echo "$a"
-a=$(bc <<< " $num1 - $num2 "); echo "$a"
-a=$(bc <<< " $num1 * $num2 "); echo "$a"
-a=$(bc <<< " scale=2; $num1 / $num2 "); echo "$a"
-a=$(bc <<< " $num1 % $num2 "); echo "$a"
-
-# Catch stdout in variable
-# cat file - gives stdout
-# a=$(cat file)
+num=27
+a=$( bc -l <<< " scale=2; sqrt($num) " ); echo "$a"
