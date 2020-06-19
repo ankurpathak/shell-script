e9496df251114c9961695f0ab86bfbbe36880fd5
Ankur Pathak <adpathak@lentra.ai>
2020-06-19 09:05:37 +0530

    Floating Arithmetic

--- hello.sh
+++ hello.sh
@@ -1,29 +1,8 @@
 #!/bin/bash
-num1=20
+num1=20.5
 num2=5
-echo $(( "$num1" + "$num2" ))
-echo $(( "$num1" - "$num2" ))
-echo $(( "$num1" * "$num2" ))
-echo $(( "$num1" / "$num2" ))
-echo $(( "$num1" % "$num2" ))
-
-echo "---------------------"
-
-echo $( expr "$num1" + "$num2" )
-echo $( expr "$num1" - "$num2" )
-echo $( expr "$num1" \* "$num2" )
-echo $( expr "$num1" / "$num2" )
-echo $( expr "$num1" % "$num2" )
-
-echo "---------------------"
-
-let " a = $num1 + $num2 "; echo "$a"
-let " a = $num1 - $num2 "; echo "$a"
-let " a = $num1 * $num2 "; echo "$a"
-let " a = $num1 / $num2 "; echo "$a"
-let " a = $num1 % $num2 "; echo "$a"
-
-let " a = 9 "; let " a++ "; echo "$a"
-let " a = 9 "; let " a-- "; echo "$a"
-let " a = 9 "; let " ++a "; echo "$a"
-let " a = 9 "; let " --a "; echo "$a"
+a=$(bc <<< " $num1 + $num2 "); echo "$a"
+a=$(bc <<< " $num1 - $num2 "); echo "$a"
+a=$(bc <<< " $num1 * $num2 "); echo "$a"
+a=$(bc <<< " scale=2; $num1 / $num2 "); echo "$a"
+a=$(bc <<< " $num1 % $num2 "); echo "$a"
