f8622d810598fa7446f2f536e3c6f40f56f52e54
Ankur Pathak <adpathak@lentra.ai>
2020-06-27 11:13:06 +0530

    Transverse String Character by Character

--- hello.sh
+++ hello.sh
@@ -14,3 +14,10 @@ echo "${str:6}";
 
 #substring extract with position and length/World only
 echo "${str:6:5}";
+
+# transverse string character by character
+name='Ankur Pathak';
+for i in $( seq 0 $(( ${#name} - 1)) ); 
+do 
+	echo "${name:$i:1}"; 
+done;
